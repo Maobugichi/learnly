f1a02cbc8c35d66c278076fd6c5074424f85d68e
Maobugichi <immansamuel846@gmail.com>
2025-03-06 23:40:05 -0800

    fix:make improvements via the drag and drop functionality6

--- src/Components/Piece.jsx
+++ src/Components/Piece.jsx
@@ -8,7 +8,7 @@ const Piece = ({ref,content,isVisible}) => {
                 ref={ref} 
                 animate={!isVisible ? {scale:0} : null}
                 exit={{opacity:0}}
-                className={`${!isVisible ? "invisible" : null}  w-full lg:w-[250px] h-[80px] rounded-lg border-dashed border-2 grid place-content-center bg-[#F3E8FF] z-10 text-black`} style={{
+                className={`${!isVisible ? "invisible" : null}  w-full lg:w-[250px] h-[80px] rounded-lg border-dashed border-2 grid place-content-center bg-[#F3E8FF] z-10 text-[10px] lg:text-sm text-black`} style={{
                     position: 'relative',
                    }}>
                      <p>{content}</p>
